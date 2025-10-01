a14c0228222266799dc7353eb4bd9c1a3324bfd9
Rene Schneider <rene.schneider@gebit.de>
2025-10-01 10:36:28 +0200

    Allow to disable the Chromium rendering pipeline
    This introduces a hook and patches to Chromium which allow the embedding
    application to disable and enable the rendering pipeline of an embedded
    browser window at will. Everything just works as it normally would
    (event handlers being called and such), but the screen doesn't update
    anymore at all until rendering is re-enabled. This allows an application to effectively load pages while still showing the last rendered frame of the previous page and wait with the first frame update until the page load was completed entirely, thus eliminating any blank (white) frames or partially rendered page frames the browser would display otherwise if the page load process takes too much time.

--- libcef/browser/chrome/chrome_browser_host_impl.h
+++ libcef/browser/chrome/chrome_browser_host_impl.h
@@ -160,6 +160,9 @@ class ChromeBrowserHostImpl : public CefBrowserHostBase {
 
   void DoCloseBrowser();
 
+  // Rendering block implementation
+  void SetRenderingBlocked(bool blocked) override;
+
   // Returns the current tab index for the associated WebContents, or
   // TabStripModel::kNoTab if not found.
   int GetCurrentTabIndex() const;
